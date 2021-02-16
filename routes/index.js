7dbe234bbfd1f2015337bc68a8f4f9f5b33285e4
Oleksandr Krokhmalnyi <magr0s.ak@gmail.com>
2021-02-16 19:54:21 +0200

    styl: Update code style

--- routes/index.js
+++ routes/index.js
@@ -2,60 +2,60 @@ const fs = require("fs")
 
 module.exports = function (app, addon) {
 
-    //fires after addon installation
-    app.all('/installed', async function (req, res, next) {
-        console.log("installation...")
-        global.database.collection(global.JiraAccountInfoStore).findOne({"installed.clientKey": req.body.clientKey}, function (err, result) {
-            if (err) console.log(err);
-            if (!result) {
-                global.database.collection(global.JiraAccountInfoStore).insertOne(req.body, async (err, res) => {
-                    if (err) throw err;
-                    next();
-                });
-            } else {
-                global.database.collection(global.JiraAccountInfoStore).updateOne({"installed.clientKey": req.body.clientKey}, {$set: req.body}, function (err, res) {
-                    next();
-                });
-            }
+  //fires after addon installation
+  app.all('/installed', async function (req, res, next) {
+    console.log("installation...")
+    global.database.collection(global.JiraAccountInfoStore).findOne({ "installed.clientKey": req.body.clientKey }, function (err, result) {
+      if (err) console.log(err);
+      if (!result) {
+        global.database.collection(global.JiraAccountInfoStore).insertOne(req.body, async (err, res) => {
+          if (err) throw err;
+          next();
         });
-    });
-
-    app.get('/', function (req, res) {
-        res.format({
-            'text/html': function () {
-                res.redirect('/atlassian-connect.json');
-            },
-            'application/json': function () {
-                res.redirect('/atlassian-connect.json');
-            }
+      } else {
+        global.database.collection(global.JiraAccountInfoStore).updateOne({ "installed.clientKey": req.body.clientKey }, { $set: req.body }, function (err, res) {
+          next();
         });
+      }
     });
+  });
 
-
-    app.get('/main-page', addon.authenticate(), async function (req, res) {
-        res.render("main-page")
+  app.get('/', function (req, res) {
+    res.format({
+      'text/html': function () {
+        res.redirect('/atlassian-connect.json');
+      },
+      'application/json': function () {
+        res.redirect('/atlassian-connect.json');
+      }
     });
+  });
 
-    app.post('/main-page', addon.checkValidToken(), async function (req, res) {
 
-    });
+  app.get('/main-page', addon.authenticate(), async function (req, res) {
+    res.render("main-page");
+  });
 
-    // load any additional files you have in routes and apply those to the app
-    {
-        var path = require('path');
-        var files = fs.readdirSync("routes");
-        for (var index in files) {
-            var file = files[index];
-            if (file === "index.js") continue;
-            // skip non-javascript files
-            if (path.extname(file) != ".js") continue;
+  app.post('/main-page', addon.checkValidToken(), async function (req, res) {
 
-            var routes = require("./" + path.basename(file));
+  });
 
-            if (typeof routes === "function") {
-                routes(app, addon);
-            }
-        }
+  // load any additional files you have in routes and apply those to the app
+  {
+    var path = require('path');
+    var files = fs.readdirSync("routes");
+    for (var index in files) {
+      var file = files[index];
+      if (file === "index.js") continue;
+      // skip non-javascript files
+      if (path.extname(file) != ".js") continue;
+
+      var routes = require("./" + path.basename(file));
+
+      if (typeof routes === "function") {
+        routes(app, addon);
+      }
     }
+  }
 };
 
