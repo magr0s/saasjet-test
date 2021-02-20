fcda7ab5eec6b318de7618ace1bdc44387e25d83
Oleksandr Krokhmalnyi <magr0s.ak@gmail.com>
2021-02-20 02:08:16 +0200

    refactor: Change cell value to counter

--- public/js/index.js
+++ public/js/index.js
@@ -135,11 +135,7 @@ function fillTableResult ($el, cols, rows) {
         jql
       } = c;
 
-<<<<<<< HEAD
-      return `<td><a href="${window.baseUrl}/issues/?jql=${jql}">${value.length}</a></td>`;
-=======
       return `<td><a href="${window.baseUrl}/issues/?jql=${jql}">${value}</a></td>`;
->>>>>>> refactor_cell_value
     });
 
     return `<tr>${cells.join('')}</tr>`;
